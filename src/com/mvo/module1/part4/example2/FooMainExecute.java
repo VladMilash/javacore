4a35ed94e7322ed03d5121a740a3a9331c81d13d
Vlad_Milas <12milash12@gmail.com>
2024-06-10 21:27:57 +0300

    implemented Foo with ExecutorService

---
+++ src/com/mvo/module1/part4/example2/FooMainExecute.java
@@ -0,0 +1,20 @@
+package src.com.mvo.module1.part4.example2;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class FooMainExecute {
+    public static void main(String[] args) {
+        Foo foo = new Foo();
+
+        ExecutorService executor = Executors.newFixedThreadPool(3);
+
+        executor.execute(foo::first);
+        executor.execute(foo::second);
+        executor.execute(foo::third);
+
+        executor.shutdown();
+
+    }
+
+}
