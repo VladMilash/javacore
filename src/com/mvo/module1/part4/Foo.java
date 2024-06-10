e21bdce164fb7115196e9e02a890b85a5705e355
Vlad_Milas <12milash12@gmail.com>
2024-06-10 11:12:55 +0300

    customization git

---
+++ src/com/mvo/module1/part4/Foo.java
@@ -0,0 +1,17 @@
+package src.com.mvo.module1.part4;
+
+public class Foo {
+
+    public void first() {
+        System.out.println("first");
+    }
+
+    public void second() {
+        System.out.println("second");
+    }
+
+    public void third() {
+        System.out.println("third");
+    }
+
+}
