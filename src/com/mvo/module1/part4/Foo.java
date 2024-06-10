d73f72f34502a60f398bfe49ec7ad576198921f7
Vlad_Milas <12milash12@gmail.com>
2024-06-10 13:12:37 +0300

    Implements Foo

--- src/com/mvo/module1/part4/Foo.java
+++ src/com/mvo/module1/part4/Foo.java
@@ -1,17 +1,39 @@
 package src.com.mvo.module1.part4;
 
+import java.util.concurrent.Semaphore;
+
 public class Foo {
+    private final Semaphore semaphoreFirst = new Semaphore(1);
+    private final Semaphore semaphoreSecond = new Semaphore(0);
+    private final Semaphore semaphoreThird = new Semaphore(0);
 
     public void first() {
-        System.out.println("first");
+        try {
+            semaphoreFirst.acquire();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.print("first");
+        semaphoreSecond.release();
     }
 
     public void second() {
-        System.out.println("second");
+        try {
+            semaphoreSecond.acquire();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.print("second");
+        semaphoreThird.release();
+
     }
 
     public void third() {
-        System.out.println("third");
+        try {
+            semaphoreThird.acquire();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.print("third");
     }
-
 }
