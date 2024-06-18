263ff6aab413d1e7a373caf678d29e3144edc55e
Vlad_Milas <12milash12@gmail.com>
2024-06-18 11:38:31 +0300

    implemented Iterator

---
+++ src/com/mvo/module1/part5/patterns/iterator/IteratorDemo.java
@@ -0,0 +1,19 @@
+package src.com.mvo.module1.part5.patterns.iterator;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class IteratorDemo {
+    public static void main(String[] args) {
+        List<Integer> numbers = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            numbers.add(i);
+        }
+        Iterator<Integer> iterator = numbers.iterator();
+        while (iterator.hasNext()){
+            Integer number = iterator.next();
+            System.out.println(number);
+        }
+    }
+}
